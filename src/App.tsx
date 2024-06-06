f9c69b58d02336c5cca0f1763817f0a8265386ab
gtianyi <2008tygu@gmail.com>
2024-06-05 21:13:25 -0400

    react learn - update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import Message from './Message'
-import ListGroup from './components/ListGroup';
+//import ListGroup  from './components/ListGroup';
+import Alert  from './components/Alert';
 function App(){
-    let items = ["NY", "SF", "TK"]
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    }
-    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+    return <div>
+        <Alert>
+            Hello <span>World</span>
+        </Alert>
+    </div>;
 }
 
 export default App;
