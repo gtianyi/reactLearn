1437b54522f9e53e1f2b7ef6a1c0f6fd609c4403
gtianyi <2008tygu@gmail.com>
2024-06-05 20:39:40 -0400

    react learn - update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
+import { MouseEvent } from "react";
 function ListGroup() {
     let items = ["NY", "SF", "TK"]
-    items = []
+
+    // Event handler
+    const handleClick = (event: MouseEvent) => console.log(event)
 
     return (
         <>
             <h1>List</h1>
             {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                {items.map((item) => (<li key={item}>{item}</li>))}
+                {items.map((item, index) => (<li className="list-group-item" key={item} onClick={handleClick}>{item}</li>))}
             </ul>
         </>
     );
