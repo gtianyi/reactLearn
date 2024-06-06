b81d5f9784ebb79dc4ea67716ba67043491d8ea9
gtianyi <2008tygu@gmail.com>
2024-06-05 21:00:18 -0400

    react learn - update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 interface Props{
     items: string[];
     heading: string;
+    // (item: string) => void
+    onSelectItem: (item: string) => void;
 
 }
-function ListGroup({items, heading}: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
 
     // Hook
     const [selectedIndex, setSelectedIndex] = useState(-1)
@@ -15,7 +17,17 @@ function ListGroup({items, heading}: Props) {
             <h1>{heading}</h1>
             {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                {items.map((item, index) => (<li className={selectedIndex === index ?"list-group-item active" : "list-group-item"} key={item} onClick={() => {setSelectedIndex(index)}}>{item}</li>))}
+                {items.map((item, index) => (
+                    <li
+                        className={selectedIndex === index ?"list-group-item active" : "list-group-item"}
+                        key={item}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                            onSelectItem(item)
+                        }}
+                    >
+                        {item}
+                    </li>))}
             </ul>
         </>
     );
