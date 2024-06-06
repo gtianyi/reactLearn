c7571641d3e740e5a4b209349893e08c16de83c8
gtianyi <2008tygu@gmail.com>
2024-06-05 20:04:29 -0400

    init

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message () {
+  // JSX: javaScript XML
+  const name = 'Tianyi';
+  if (name)
+     return <h1>Hello {name}</h1>;
+  return <h1>Hello World{name}</h1>;
+}
+
+export default Message;
